6779b41dfa3ab838565d7023939597e7370605a2
AlexGeamanu <p2665879@my365.dmu.ac.uk>
2026-08-17 16:23:48 +0100

    Adding Spring boot skeleton and connection to Postgress

---
+++ backend/VenueWebAppProject/src/main/java/com/example/venuewebappproject/VenueWebAppProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.venuewebappproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VenueWebAppProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VenueWebAppProjectApplication.class, args);
+    }
+
+}
---
+++ backend/VenueWebAppProject/src/test/java/com/example/venuewebappproject/VenueWebAppProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.venuewebappproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VenueWebAppProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
