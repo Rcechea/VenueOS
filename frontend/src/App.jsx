f32153d0e7d611936bbb7e4c887dd0c60ce13dd1
Rcechea <razvancechea2@gmail.com>
2026-09-01 22:45:23 +0100

    nginx reverse proxy set up, backend changed to connect http address, cors changed to os http

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -19,7 +19,7 @@ function App() {
     setError("");
 
     try {
-      const response = await fetch("http://localhost:8080/api/auth/login", {
+      const response = await fetch("https://venue-api.wotaro.co.uk/api/auth/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -112,7 +112,7 @@ function App() {
     setRegSuccess("");
 
     try {
-      const response = await fetch("http://localhost:8080/api/auth/register", {
+      const response = await fetch("https://venue-api.wotaro.co.uk/api/auth/register", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
